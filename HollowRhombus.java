92c2a631c8b79e3f53571295b8370afa9b967c82
ParthBaldaniya1242 <133247721+ParthBaldaniya1242@users.noreply.github.com>
2023-05-11 21:48:12 +0530

    Add files via upload

---
+++ HollowRhombus.java
@@ -0,0 +1,21 @@
+public class HollowRhombus {
+    public static void Hollow_Rhombus(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n+1; j++){
+                if(i==1 || i==n || j==1 || j==n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Hollow_Rhombus(5);
+    }
+}
